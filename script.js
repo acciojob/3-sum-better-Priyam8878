fe5b75c010a4be8cd694def796141553715d1416
Priyam8878 <112754436+Priyam8878@users.noreply.github.com>
2023-06-15 19:46:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function threeSum(arr, target) {
-// write your code here
+// write your code
+	for(let i=0;i<arr.length;i++){
+		
+		let twoSum(i+1,arr.length-1,arr,target-arr[i])
+	}
+
   
+}
+function twoSum(i, j, arr, target) {
+	let sum=0;
+	let a= arr[i-1]
+	while (i<j) {
+		if(arr[i]+arr[j]>target){
+			j--;
+		}else if(arr[i]+arr[j]<target) {
+			i++;
+			
+		}else{
+			sum=arr[i]+arr[j]+a;
+		}
+	}
+	return sum;
 }
 
 module.exports = threeSum;
